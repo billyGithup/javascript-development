ce92a545f9811b9d44cd6d0d7c8951ce446a34eb
billyGitHup <billy.meas@yahoo.com>
2024-11-20 15:59:41 -0600

    Added a new file

---
+++ introToJavaScript.js
@@ -0,0 +1,17 @@
+// The variable below has a value of 10. This is a one line comment.
+
+/* This is a
+multi-line comment.
+*/
+
+// int is a whole number data type. So 30 is an int data type.
+const variableA = 30;
+let variableB = 40;
+
+console.log(variableA);
+console.log(variableB);
+variableB = "Hello, world";
+console.log(variableB);
+
+let variableC;
+console.log(variableC);
