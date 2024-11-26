952302ac5b68290542cd412d7651de27ed0e8c22
billyGitHup <billy.meas@yahoo.com>
2024-11-26 16:07:18 -0600

    Number lab

---
+++ number.js
@@ -0,0 +1,50 @@
+const wholeNumber = 33; // Integer number
+const floatNumber = 34.99; // Positive float number
+const negativeFloat = -10.99; // Negative float number
+
+const maximumFloatNumber = 9.11111111111111111;
+// console.log(maximumFloatNumber);
+
+const bigIntNum = 25562356573457347347n;
+// console.log(bigIntNum);
+
+const binaryNum = 0b11010; // Binary number
+const hexadecimalNum = 0x1a; // Hexadecimal number
+const octalNum = 0o32; // Octal number
+
+// console.log(binaryNum);
+// console.log(hexadecimalNum);
+// console.log(octalNum);
+
+// Exponential literals
+const exponent1 = 0e-3;
+const exponent2 = 0e5;
+const exponent3 = 4e4;
+const exponent4 = 234e-4;
+const exponent5 = 5e-50;
+
+// console.log(exponent1);
+// console.log(exponent2);
+// console.log(exponent3);
+// console.log(exponent4);
+// console.log(exponent5);
+
+// Numeric separators
+const separator1 = 9_563.25;
+const separator2 = 0b1010_0001;
+const separator3 = 0xa0_b0_c0;
+
+// console.log(separator1);
+// console.log(separator2);
+// console.log(separator3);
+
+// Static Number properties
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+
+// Static Number methods
+// console.log(Number.isNaN(20e-10));
+// console.log(Number.isInteger(2.0));
+console.log(Number.parseInt(10.999));
