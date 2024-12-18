ed721f8062b3ad9d8ebf66148a6614e57bc9c5ce
billyGitHup <billy.meas@yahoo.com>
2024-12-18 16:03:56 -0600

    Intro to web lap part 2

--- IntroToWeb/scripts.js
+++ IntroToWeb/scripts.js
@@ -5,3 +5,16 @@ document.getElementById("first").innerText =
 const pElement = document.getElementById("first");
 pElement.style.color = "red";
 pElement.style.fontSize = "25px";
+
+const elements = document.getElementsByClassName("cool-class");
+console.log(elements);
+
+const elementsTwo = document.querySelectorAll(".cool-class");
+console.log(elementsTwo);
+
+elementsTwo.forEach((element) => {
+  element.style.color = "orange";
+});
+
+const pEl = document.querySelectorAll("p");
+console.log(pEl);
