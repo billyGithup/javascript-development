738d4ffb7d9449c5e4d10c8d59b57f02ad61534c
Yada Meas <yada@snapit.solutions>
2024-12-30 16:03:25 -0600

    Sandwich Shop App part 1

---
+++ sandwichShop/app.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const shopTitle =
+  "===========================================" + "\n||\t\t\t\t\t ||";
+
+console.log(shopTitle);
---
+++ sandwichShop/ingredients/cheeses.js
@@ -0,0 +1,6 @@
+exports.cheeses = {
+  American: 1.99,
+  Asiago: 2.99,
+  "Blue Cheese": 3.99,
+  "Pepper Jack": 4.99
+};
