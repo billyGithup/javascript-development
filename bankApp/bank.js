4c6575709520181ecb89af0a99230ebf9a83bb47
billyGitHup <billy.meas@yahoo.com>
2025-01-06 16:04:20 -0600

    Bank App part 2

--- bankApp/bank.js
+++ bankApp/bank.js
@@ -1,13 +1,18 @@
 const prompt = require("prompt-sync")({ sigint: true });
 const chalk = require("chalk");
 const fs = require("fs");
-const { checkYorN, validateNewAcctInputs } = require("./helpers.js");
+const {
+  checkYorN,
+  validateNewAcctInputs,
+  handleMenu
+} = require("./helpers.js");
 
 const bankTitle =
   "===========================================" +
   "\n||\t\t\t\t\t ||\n||\t         ABC BANK\t\t ||" +
   "\n||\t\t\t\t\t ||\n" +
   "===========================================";
+const menu = `What would you like to do?\n1) Show balance\n2) Withdraw\n3) Deposit\n4) Show account information\n5) Delete account\n6) Exit\n`;
 let allAccounts = [];
 
 if (fs.existsSync("./accountDatabase.txt")) {
@@ -49,7 +54,27 @@ if (userInput.toLowerCase() == "n") {
       allAccounts
     );
 
-    // allAccounts.push(newlyCreatedAccount);
-    // console.log(chalk.green("\nYour account has been created successfully!\n"));
+    allAccounts.push(newlyCreatedAccount);
+    console.log(chalk.green("\nYour account has been created successfully!\n"));
+    console.log(menu);
+    let menuInput = prompt("Menu number: ");
+    while (menuInput !== "6") {
+      const result = handleMenu(menuInput, newlyCreatedAccount, allAccounts);
+      if (result == "exit") break;
+      console.log(menu);
+      menuInput = prompt("Menu number: ");
+      if (menuInput == "6") {
+        fs.writeFile(
+          "./accountDatabase.txt",
+          JSON.stringify(allAccounts),
+          (error) => {
+            if (error) throw error;
+            console.log(
+              chalk.green("\nThank you for visiting us. Have a nice day!")
+            );
+          }
+        );
+      }
+    }
   }
 }
--- bankApp/helpers.js
+++ bankApp/helpers.js
@@ -1,4 +1,15 @@
 const prompt = require("prompt-sync")({ sigint: true });
+const chalk = require("chalk");
+const fs = require("fs");
+
+const acctNumExists = (acctNumber, acctRecords) => {
+  // const foundAcct = acctRecords.find(
+  //   (account) => account.accountNumber == acctNumber
+  // );
+  return acctRecords.find((account) => account.accountNumber == acctNumber)
+    ? true
+    : false;
+};
 
 exports.checkYorN = (input) => "ynYN".includes(input) && input.length == 1;
 
@@ -19,7 +30,93 @@ exports.validateNewAcctInputs = (
     balance = prompt("Please enter a whole balance amount only: ");
   }
   while (!(/^[0-9]*$/.test(accountNumber) && accountNumber.length >= 5)) {
-    accountNumber = prompt("Please enter at least five-digit account numbers: ");
+    accountNumber = prompt(
+      "Please enter at least five-digit account numbers: "
+    );
   }
-  
+  while (acctNumExists(accountNumber, allAccounts)) {
+    accountNumber = prompt(
+      chalk.red("Account number already exists. Please enter a different one: ")
+    );
+
+    while (!(/^[0-9]*$/.test(accountNumber) && accountNumber.length >= 5)) {
+      accountNumber = prompt(
+        chalk.red("Please enter at least five-digit account numbers: ")
+      );
+    }
+  }
+
+  return {
+    firstName,
+    lastName,
+    balance,
+    accountNumber,
+    createdAt: new Date().toLocaleDateString()
+  };
+};
+
+exports.handleMenu = (menuNumber, account, accountRecords) => {
+  switch (menuNumber) {
+    case "1":
+      console.log(
+        chalk.green("\nYour current balance is $" + account.balance + ".\n")
+      );
+      break;
+    case "2":
+      let withdrawAmount = prompt("How much would you like to withdraw? ");
+      while (!(/^[0-9]*$/.test(withdrawAmount) && withdrawAmount)) {
+        withdrawAmount = prompt("Please enter only a whole amount: ");
+      }
+
+      if (parseInt(withdrawAmount) > parseInt(account.balance)) {
+        console.log(
+          chalk.red(
+            `\nYou do not have enough in your account to withdraw $${withdrawAmount}.\n`
+          )
+        );
+      } else {
+        console.log(chalk.green("\nWithdraw: $" + withdrawAmount));
+        account.balance = parseInt(account.balance) - parseInt(withdrawAmount);
+        console.log(
+          chalk.green("Your current balance is $" + (account.balance + ".\n"))
+        );
+      }
+      break;
+    case "3":
+      let depositAmount = prompt("How much would you like to deposit? ");
+      while (!(/^[0-9]*$/.test(depositAmount) && depositAmount)) {
+        depositAmount = prompt("Please enter only a whole amount: ");
+      }
+
+      console.log(chalk.green("\nDeposit: $" + depositAmount));
+      account.balance = parseInt(account.balance) + parseInt(depositAmount);
+      console.log(
+        chalk.green("Your current balance is $" + account.balance) + ".\n"
+      );
+      break;
+    case "4":
+      console.log(
+        chalk.green(
+          `\nName: ${account.firstName} ${account.lastName}\nAccount Number: ${account.accountNumber}\nBalance: $${account.balance}\nCreated: ${account.createdAt}\n`
+        )
+      );
+      break;
+    case "5":
+      const newAcctRecords = accountRecords.filter(
+        (eachAccount) => eachAccount.accountNumber != "12345"
+        // (eachAccount) => eachAccount.accountNumber != account.accountNumber
+      );
+      fs.writeFile(
+        "./accountDatabase.txt",
+        JSON.stringify(newAcctRecords),
+        (err) => {
+          if (err) throw err;
+          console.log(chalk.red("\nAccount deleted successfully.\n"));
+        }
+      );
+      return "exit";
+    default:
+      console.log(chalk.red("\nThat is not a valid menu number.\n"));
+      break;
+  }
 };
